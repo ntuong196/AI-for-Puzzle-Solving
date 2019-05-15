78462c8c0a83602eb3f74fcebf5119cd289e478a
ntuong196 <stranger0411@gmail.com>
2019-05-15 16:48:59 +1000

    Add week8 to week 11

--- Week7-Formula-puzzle/formula_puzzle.py
+++ Week7-Formula-puzzle/formula_puzzle.py
@@ -8,7 +8,9 @@
 #    and str.maketrans  are handy functions
 #    Using the 're' module leads to more concise code.
 
-import re, itertools
+import re
+import itertools
+
 
 def solve(formula):
     """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
@@ -16,27 +18,29 @@ def solve(formula):
     for f in fill_in(formula):
         if valid(f):
             return f
-    
+
+
 def fill_in(formula):
     ''' Return a generator that enumerate  all possible fillings-in
         of letters in formula with digits.'''
 
-    ## INSERT YOUR CODE HERE
+    # INSERT YOUR CODE HERE
 
-    
+
 def valid(f):
     """Formula f is valid if and only if it has no 
     numbers with leading zero, and evals true."""
-    try: 
+    try:
         return not re.search(r'\b0[0-9]', f) and eval(f) is True
     except ArithmeticError:
         return False
 
-print( solve('ODD + ODD == EVEN'))
-print (solve('A**2 + BC**2 == BD**2'))
 
-##    Should output
-##    >>> 
+print(solve('ODD + ODD == EVEN'))
+print(solve('A**2 + BC**2 == BD**2'))
+
+# Should output
+# >>>
 ##    655 + 655 == 1310
 ##    2**1 + 34**1 == 36**1
 
